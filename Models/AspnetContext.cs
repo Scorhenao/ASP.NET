db19216116edcee032b9c14456d95c3b987484ff
Scorhenao <samcorhenao@gmail.com>
2025-10-29 16:20:04 -0500

    second commi

---
+++ Models/AspnetContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNET.Models;
+
+public partial class AspnetContext : DbContext
+{
+    public AspnetContext()
+    {
+    }
+
+    public AspnetContext(DbContextOptions<AspnetContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Data Source=ScoreLaptop\\MSSQLSERVER01;Initial Catalog=ASPNET;Integrated Security=True;Trust Server Certificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Usuarios__3213E83F3EC30C0E");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Clave).HasMaxLength(255);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ASPNET.Models;
+
+public partial class Usuario
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public DateOnly Fecha { get; set; }
+
+    public string Clave { get; set; } = null!;
+}
